323fbde29531c0782ce19d0f9f8f080dd7c95e4c
Chris <chris@starforge.co.uk>
2013-09-22 00:27:52 +0100

    And another.

--- marksnapshot.pl
+++ marksnapshot.pl
@@ -80,7 +80,7 @@ if(scalar(@ARGV) == 3) {
                 my $base = $config -> {"directory.$ARGV[1]"} -> {"base"};
                 $base = $config -> {"server"} -> {"base"} if(!$base);
 
-                my $mountpoint = path_join($base, $config -> {"directory.$id"} -> {"remotedir"});
+                my $mountpoint = path_join($base, $config -> {"directory.$ARGV[1]"} -> {"remotedir"});
 
                 # Okay, make sure that the directory exists
                 if(-d $mountpoint) {
